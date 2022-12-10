85af15a22ec4e76faca33c6934db35a173c76f09
Jonah Meijers <jonahmeijers97@gmail.com>
2022-12-10 23:44:14 +0100

    2022 day 10 optimisations

--- 2022/src/bin/day_ten.rs
+++ 2022/src/bin/day_ten.rs
@@ -4,27 +4,23 @@ fn main() -> Result<()> {
     let input = aoc::load_input("ten")?;
     let mut cycle: u32 = 0;
     let mut register: i32 = 1;
-    let mut signal_strengths: Vec<i32> = vec![];
+    let mut signal_strengths: i32 = 0;
     let mut crt: [[bool; 40]; 6] = [[false; 40]; 6];
     for l in input.lines() {
         match l.split(' ').next().unwrap_or("") {
             "noop" => {
-                cycle += 1;
-                clock_cycle(cycle, register, &mut signal_strengths, &mut crt);
+                cycle = clock_cycle(cycle, register, &mut signal_strengths, &mut crt);
             }
             "addx" => {
-                cycle += 1;
-                clock_cycle(cycle, register, &mut signal_strengths, &mut crt);
-                cycle += 1;
+                cycle = clock_cycle(cycle, register, &mut signal_strengths, &mut crt);
                 register += l.split(' ').last().unwrap().parse::<i32>().unwrap();
-                clock_cycle(cycle, register, &mut signal_strengths, &mut crt);
+                cycle = clock_cycle(cycle, register, &mut signal_strengths, &mut crt);
             }
             _ => bail!("Invalid line"),
         };
     }
-    println!("Part one: {}", signal_strengths.iter().sum::<i32>());
-    println!("Part two: |");
-    println!("          v");
+    println!("Part one: {signal_strengths}");
+    println!("Part two: \u{2193}");
     print_crt(&crt);
 
     Ok(())
@@ -33,27 +29,23 @@ fn main() -> Result<()> {
 fn clock_cycle(
     cycle: u32,
     register: i32,
-    signal_strengths: &mut Vec<i32>,
+    signal_strengths: &mut i32,
     crt: &mut [[bool; 40]; 6],
-) {
-    handle_signal_strengths(cycle + 1, register, signal_strengths);
+) -> u32 {
+    let current_cycle = cycle + 1;
 
-    let idx = crt_index(cycle);
+    // Part one
+    if (current_cycle + 1) % 40 == 20 {
+        *signal_strengths += register * (current_cycle + 1) as i32;
+    }
+
+    // Part two
+    let idx = crt_index(current_cycle);
     crt[idx.0][idx.1] = (register - 1..=register + 1).contains(&(idx.1 as i32));
+    current_cycle
 }
 
-fn handle_signal_strengths(cycle: u32, register: i32, signal_strengths: &mut Vec<i32>) {
-    match cycle {
-        20 => signal_strengths.push(register * cycle as i32),
-        60 => signal_strengths.push(register * cycle as i32),
-        100 => signal_strengths.push(register * cycle as i32),
-        140 => signal_strengths.push(register * cycle as i32),
-        180 => signal_strengths.push(register * cycle as i32),
-        220 => signal_strengths.push(register * cycle as i32),
-        _ => (),
-    };
-}
-
+#[inline]
 fn crt_index(cycle: u32) -> (usize, usize) {
     (cycle as usize / 40 % 6, cycle as usize % 40)
 }
